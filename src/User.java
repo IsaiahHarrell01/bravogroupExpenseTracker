61798ca0a7b677668169a03c431c64acd373ab5e
Nizar Youssef <nbyoussef@csus.edu>
2025-04-22 17:52:14 -0700

    Add functions and attributes to User class

--- src/User.java
+++ src/User.java
@@ -1,3 +1,33 @@
 public class User {
+    private String username;
+    private String password;
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public FinancialSummary getFinancialSummary(){
+        //add code to put the correct data into these variables
+        double totalIncome;
+        double totalExpenses;
+        double remainingBalance;
+        return new FinancialSummary(totalIncome, totalExpenses, remainingBalance);
+    }
+    public void addIncome(){
 
+    }
+    public void editIncome(){
+
+    }
+    public void deleteIncome(){
+
+    }
+    public void addExpense(){
+
+    }
+    public void editExpense(){
+
+    }
+    public void deleteExpense(){
+
+    }
 }
